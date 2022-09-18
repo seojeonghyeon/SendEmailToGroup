56df402427ad52f935ec0e769a0b8794f3f30100
seojeonghyeon <seojeonghyeon0630@gmail.com>
2022-09-18 11:43:15 +0900

    add test code about checked-email part

--- src/test/java/com/zayden/sendemailtogroup/controller/AccountControllerTest.java
+++ src/test/java/com/zayden/sendemailtogroup/controller/AccountControllerTest.java
@@ -1,15 +1,22 @@
 package com.zayden.sendemailtogroup.controller;
 
 import com.zayden.sendemailtogroup.dao.AccountRepository;
+import com.zayden.sendemailtogroup.domain.AccountEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,6 +25,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
 
     @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
@@ -29,6 +42,21 @@ class AccountControllerTest {
                 .andExpect(model().attributeExists("signUpForm"));
     }
 
-    
+    @DisplayName("회원 가입 처리 - 입력 값 정상")
+    void signUpSubmit_with_correct_input() throws Exception{
+        mockMvc.perform(post("/sign-up")
+                .param("nickname","jeonghyeon")
+                .param("email","seojeonghyeon0630@gmail.com")
+                .param("password","12345").with(csrf()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        AccountEntity accountEntity = accountRepository.findByEmail("seojeonghyeon0630@gmail.com");
+        assertNotNull(accountEntity);
+        assertNotEquals(accountEntity.getPassword(), "12345678");
+        assertNotNull(accountEntity.getEmailCheckToken());
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
+    }
 
 }
